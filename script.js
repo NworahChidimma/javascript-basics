81bf89986d47311492a6fce1aad2a96163957853
NworahChidimma <winnernworah@gmail.com>
2025-08-12 15:07:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
- let name = "Chidimmma"
+// VARIABLES//
+
+var favoriteColor = "red"
+let name = "Chidimma"
+const PI = 3.14
+// console.log allows you to see your code in your console
+ console.log(name);
+
+ /*DATA TYPES
+ 1.Primitive data type: There are 5;
+ 2. Reference data type */
+
+
+// PRIMITIVE//
+ // string: words//
+ let favoriteMovie = "Harry Potter"
+  //numbers//
+  let age = 20
+  //boolean//
+  age = true
+
  
