69111f77ef9e66724e7ad108c5532e62c15fc109
NworahChidimma <winnernworah@gmail.com>
2025-08-13 08:46:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -19,4 +19,21 @@ const PI = 3.14
   //boolean//
   age = true
 
+  //Functions//
+  //function declaration syntax://
+
+  function sum (num1,num2){
+    if (num2 === undefined){
+        return num1 
+    }
+    return num1 + num2;
+  }
+  console.log(sum(2,4))
+  console.log(sum(2))
+   
+  // we can also have an anonymous function or arrow function//
+  const add = (num1,num2) => {
+    return num1 + num2
+  }
+  console.log(add(1,2))
  
