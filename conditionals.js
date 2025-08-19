e73d8ed4800a83e6352c995b090849c3916a5f57
NworahChidimma <winnernworah@gmail.com>
2025-08-19 20:57:04 +0100

    Update conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -1,1 +1,14 @@
 //CONDITIONALS: these are simply decision making abilities
+
+let chiObj = {
+    name: "Chidimma",
+    age: 20,
+    nationality: "Nigerian"
+}
+// check if you are eligible to vote in an election in Nigeria
+if(chiObj.nationality == "Nigerian" && chiObj.age >= 18) {
+console.log("You are eligible to vote")
+}
+else{
+console.log("You are not eligible to vote")
+}
